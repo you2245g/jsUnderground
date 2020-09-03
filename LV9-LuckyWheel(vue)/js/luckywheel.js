47cdd53ce879d4450ca8b9eb29a3bccc8f31e710
Hsieh <you2245g@gmail.com>
2020-09-03 16:34:23 +0800

    中段完成

--- LV9-LuckyWheel(vue)/js/luckywheel.js
+++ LV9-LuckyWheel(vue)/js/luckywheel.js
@@ -1,85 +1,84 @@
-new Vue({
+var nn = new Vue({
   el:'#wrapper',
   data: {
-      url:'', 
-      prizes: [],      //json置放容器
+      //for外觀
+      url:'./luckywheel2017.json', //設置初始url值
+      award:'',        //印出得獎獎品 
+      prizes:[],       //json資料放置
+      deg:0,           //for角度計算
+      innerObject:0,   //內容物件（分6面及20面）
+      //for內部運算
       prizeNumber:[],  //獎號陣列
       prizeDegree:[],  //中獎角度陣列      
-      innerObject:0,   //內容物件（分6面及20面）
       chanceRemain:0,  //轉動次數
-      startDegree:0,   //指針起使角度(預設為0)
-      clicked:false,   //禁止再點按觸動
-      isShow:false,
+
+      startDegree:0,   //指針起始角度(預設為0)
+      clicked:true,   //禁止再點按觸動
+      isShow:false,    //顯示中獎畫面控制(加入css、取消及顯現prize面版)
     },
   mounted(){
-    this.url = './luckywheel2017.json',
-    this.initPrize(this.url)
+    this.initPrize(this.url)  //初始資料建立
   },
   methods: {
-    //引入Json資料
+    //引入JSON資料
     initPrize(url) {
       var vm = this  //重要!!!
       axios.get(url)
         .then(function (response) {
-          vm.prizes = JSON.parse(response.request.responseText) //產生資料陣列
-          vm.innerObject = vm.prizes.length                     //動態產生內容物件數量(6 or 20)
-          vm.fanData(vm.innerObject, vm.prizes)                 //資料化外觀 & 產生獎項角度陣列
-          vm.prizeArray()                                       //動態產生獎號陣列
-          vm.drawChance(vm.prizes)                              //定義轉動次數(20次及120次)
+          //for外觀
+          vm.prizes = JSON.parse(response.request.responseText) 
+          vm.innerObject = vm.prizes.length     //動態產生內容物件數量(6 or 20)
+          vm.fan(vm.innerObject)                //資料化外觀
+          //for內部運算
+          vm.prizeArray()                       //產生獎號陣列  
+          vm.drawChance(vm.prizes)              //產生抽獎次數(20次及120次)
+          vm.generatePrizeDegree()              //產生中獎角度陣列
         })
         .catch(function (error) {
           console.log(error);
         });
+    }, 
+    //點擊更換資料
+    clickYear(url){
+      this.initPrize(url)
+      this.url = url
     },
-    //處理扇型資料
-    fanData(num, data){
-      const prizeSet = document.querySelector('.prizeSet')
+    //處理扇型
+    fan(num){
       const wheelInner = document.querySelector('.wheelInner')
       let vm = this
       let str = ''
-      let strFan = ''
-      let deg = 360 / num
-      let tilt = deg / 2
+      vm.deg = 360 / num
+      let tilt = vm.deg / 2
       //處理扇型(6面及20面)
       for(let i = 0;i<num;i++){
-        strFan +=
+        str +=
               `
-              <div class="wheelInnerSet" style="transform: rotate(${(deg*i)-tilt}deg) skewY(${deg - 90}deg);"></div>
+              <div class="wheelInnerSet" style="transform: rotate(${(vm.deg*i)-tilt}deg) skewY(${vm.deg - 90}deg);"></div>
               `
-            
       }
-      wheelInner.innerHTML = strFan;
-      //扇型內資料處理(分析2017與2018資料)
-      if(num === 6){  //假設使用2017json時
-        data.forEach(function(item,index){
-            str +=
-            `
-            <div class="prizeSetInner" style="transform: rotate(${(deg*index)}deg);">
-              <i class="material-icons">${item.icon}</i>
-              <p>${item.name}</p>
-              <div>${item.amount}</div>
-            </div>
-            `
-        });
-      }else if(num === 20){  //假設使用2018json時
-        data.forEach(function(item,index){
-          str +=
-          `
-          <div class="prizeSetInner" style="transform: rotate(${(deg*index)}deg);">
-            <span>${item.name}</span>
-            <div>${item.amount}</div>
-          </div>
-          `
-        });
-      }
-      prizeSet.innerHTML = str;
-      vm.generatePrizeDegree(deg, num) //產生角度陣列
+      wheelInner.innerHTML = str;
     },
+    //資料處理角度問題
+    setDegree(key){
+      let obj = `transform: rotate(${this.deg*key}deg)`
+      return obj
+    },
+
     //動態產生獎號陣列(prizeNumber//[0,1,2,3,4,5,6,....])
     prizeArray(){
+      this.prizeNumber = []
       for(let i = 0 ;i <this.innerObject;i++){
         this.prizeNumber.push(i)
       } 
+    },
+    //處理中獎角度陣列([0,60,120,....])
+    generatePrizeDegree(){
+      this.prizeDegree = []
+      for(var i = 0;i<this.innerObject;i++){
+        let eachDeg = this.deg * i
+        this.prizeDegree.push(eachDeg)
+      }
     },
     //動態產生抽獎次數(20次及120次)
     drawChance(data){
@@ -91,23 +90,25 @@ new Vue({
       //陣列相加
       this.chanceRemain = array.reduce((a,b)=>a+b) 
     },
-    //處理中獎角度陣列([0,60,120,....])
-    generatePrizeDegree(deg, num){
-      if(this.prizeDegree !==[]){
-        this.prizeDegree = []
-      }
-      for(var i = 0;i<num;i++){
-        let eachDeg = deg * i
-        this.prizeDegree.push(eachDeg)
-      }
-    },
+
+    //for旋轉功能
     //點擊旋轉
     rotatePrize(){
-      let vm = this 
-      //確認執行
-      //vm.clicked = !vm.clicked
+      let vm = this
+      if(!vm.clicked){return}  //避免重覆點按
       //抽獎次數減一
-      //vm.chanceRemain --
+      vm.chanceRemain --
+      //確認執行
+      vm.clicked = !vm.clicked
+      //點擊關閉中獎畫面(含css添加後移除)
+      vm.isShow = false
+      const wheelInnerSet = document.querySelectorAll('.wheelInnerSet')
+      const prizeSetInner = document.querySelectorAll('.prizeSetInner')
+      for(i = 0; i <vm.innerObject;i++){
+        wheelInnerSet[i].className = 'wheelInnerSet'
+        prizeSetInner[i].className = 'prizeSetInner'
+      }
+      //執行旋轉功能
       vm.checkPrize()
 
 
@@ -128,18 +129,19 @@ new Vue({
       const pointer = document.querySelector('.pointer')
       pointer.style.transform = `rotate(${degree}deg) `
 
-
-
       //旋轉到的部份，加上active css
+      //三秒後，獲獎顯示設定(css外觀變更，獎別顯示變更，獎品顯示品項減一)
       const wheelInnerSet = document.querySelectorAll('.wheelInnerSet')
       const prizeSetInner = document.querySelectorAll('.prizeSetInner')
       setTimeout(function(){
         wheelInnerSet[random].classList.add('active')
         prizeSetInner[random].classList.add('active2')
+        vm.award = vm.prizes[random].name
+        vm.prizes[random].amount --
         vm.isShow = true
+        vm.clicked = true
       },3000)
-
-      //產生獎別
+      
 
 
 
